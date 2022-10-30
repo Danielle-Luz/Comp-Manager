ac9a080946603e3590246c53a69824363caa4c2b
Axionauta <daniln2209@gmail.com>
2022-10-30 19:54:11 -0300

    feat: create get form data function

---
+++ src/scripts/forms.js
@@ -0,0 +1,20 @@
+export function getFormData (id, foo) {
+  const form = document.getElementById(id);
+
+  form.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const fields = form.querySelectorAll("input, select");
+
+    let fieldsData = {};
+
+    fields.forEach(({id, value}) => {
+      fieldsData = {
+        ...fieldsData,
+        [id]: value
+      }
+    });
+
+    await foo(fieldsData);
+  });
+}
