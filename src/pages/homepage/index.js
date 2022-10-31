690ea9e4da84adc9e25aeffd6c919ecb24abbdd3
Axionauta <daniln2209@gmail.com>
2022-10-31 10:42:10 -0300

    feat: invoke render options

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,6 +1,10 @@
-import { createCompanyCard, renderAllCards } from "../../scripts/render.js";
-import { getAllCompanies } from "../../scripts/requests.js";
+import { createCompanyCard, createCompanySelectOption, renderAllCards } from "../../scripts/render.js";
+import { getAllCompanies, getAllSectors } from "../../scripts/requests.js";
 
 const companies = await getAllCompanies();
 
+const sectors = await getAllSectors();
+
 renderAllCards(companies, ".organizations-list", createCompanyCard);
+
+renderAllCards(sectors, "#sectors", createCompanySelectOption);
