c86b67ff44c27d082a310ac278ba363f69b2de54
Axionauta <daniln2209@gmail.com>
2022-10-31 17:19:58 -0300

    feat: create set user info function

--- src/pages/user-dashboard/index.js
+++ src/pages/user-dashboard/index.js
@@ -3,3 +3,18 @@ import { getUserInfo, redirectUser, verifyUserLevel } from "../../scripts/reques
 redirectUser();
 
 await verifyUserLevel(false);
+
+await setUserInfo();
+
+async function setUserInfo () {
+  const userInfo = await getUserInfo();
+
+  const email = document.getElementById("user-email");
+  const professionalLevel = document.getElementById("user-level");
+  const typeOfWork = document.getElementById("user-work");
+
+  email.innerText = userInfo.email;
+  professionalLevel.innerText = userInfo.professional_level || "";
+  typeOfWork.innerText = userInfo.kind_of_work || "";
+}
+
