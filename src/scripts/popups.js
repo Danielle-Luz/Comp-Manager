9eb16f5e6f628740f470577895fad646741f4335
Axionauta <daniln2209@gmail.com>
2022-10-31 00:35:54 -0300

    feat: add class type paramenter

--- src/scripts/popups.js
+++ src/scripts/popups.js
@@ -1,8 +1,8 @@
-export function createToast (message) {
+export function createToast (message, type) {
   const articleToast = document.createElement("article");
   const articleMessage = document.createElement("p");
 
-  articleToast.classList = "full-width toast toast-sucess";
+  articleToast.classList = `full-width toast toast-${type}`;
   articleMessage.classList = "text-4";
 
   articleMessage.innerHTML = message;
