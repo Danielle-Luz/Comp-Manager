feab7f99892ff2183df7645fcd9acbea93136533
Axionauta <daniln2209@gmail.com>
2022-10-31 17:23:35 -0300

    feat: set username

--- src/pages/user-dashboard/index.js
+++ src/pages/user-dashboard/index.js
@@ -9,10 +9,12 @@ await setUserInfo();
 async function setUserInfo () {
   const userInfo = await getUserInfo();
 
+  const username = document.getElementById("username");
   const email = document.getElementById("user-email");
   const professionalLevel = document.getElementById("user-level");
   const typeOfWork = document.getElementById("user-work");
 
+  username.innerText = userInfo.username;
   email.innerText = userInfo.email;
   professionalLevel.innerText = userInfo.professional_level || "";
   typeOfWork.innerText = userInfo.kind_of_work || "";
